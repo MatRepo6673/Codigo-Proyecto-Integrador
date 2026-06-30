5b152f59fe9a233718f561593e9412731423b113
MatRepo <mathiasreyesantunez@gmail.com>
2026-06-30 00:11:34 -0600

    Implement error handling for user name lookup
    Added error handling for non-existent names and closed the scanner.

--- Codigo Proyecto Integrador/prueba.java
+++ Codigo Proyecto Integrador/prueba.java
@@ -9,13 +9,19 @@ public class prueba {
     public static void main(String[] args) throws Exception{
         File usuarios = new File("usuarios.txt");
         Scanner userScanner = new Scanner(usuarios);
-        final String input = userScanner.nextLine().toString();
+        String input = userScanner.nextLine().toString();
+        int user;
         List<String> names = Arrays.stream(input.split("\\s*,\\s*")).collect(Collectors.toList());
         ArrayList<String> nameList = new ArrayList<>();
         for (String name : names) {
             nameList.add(name.trim());
         }
         System.out.println(nameList);
-        System.out.println(nameList.get(nameList.indexOf("waka")));
+        user = nameList.indexOf("mathi");
+        if(user < 0){
+            System.out.println("error, name does not exist");
+        }else System.out.println(nameList.get(user) + nameList.indexOf("mathi"));
+
+        userScanner.close();
     }
 }
